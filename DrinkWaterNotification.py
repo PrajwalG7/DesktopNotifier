e3250b1b777f8ba8c3f05397b3c6d03099753bab
PrajwalG7 <gaikwadaryan688@gmail.com>
2021-07-23 16:18:32 +0530

    initial commit.

---
+++ DrinkWaterNotification.py
@@ -0,0 +1,22 @@
+import time
+from plyer import notification
+
+
+if __name__=='__main__':
+   title="DRINK WATER NOW!!!"
+
+   message="An adequate daily fluid intake is:  " \
+           " About 15.5 cups (3.7 liters) of fluids a day for men." \
+           " About 11.5 cups (2.7 liters) of fluids a day for women."
+
+   IconURL="C:\\Users\Aryan\PycharmProjects\pythonProject\FirstDirect\PythonProjects\DesktopAutomation\Icons8-Ios7-Industry-Water.ico"
+
+   # on loop run
+   while True:
+      notification.notify(
+       title=title,
+       message=message,
+       app_icon=IconURL,
+       timeout=5,
+      )
+      time.sleep(60*60)
